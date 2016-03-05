3224af877225ae323bf6c761e368bb8038358ab1
Ali Reza Barkhordari <abforce@live.com>
2016-03-05 19:54:54 +0330

    project info added

---
+++ cordova-guard.js
@@ -0,0 +1,10 @@
+/**
+* Cordova Guard
+*	A JavaScript based access control solution for protecting
+*	Cordova JavaScript APIs against malicious JavaScript codes
+*
+* Author		Ali Reza Barkhordari
+* Start Date	Saturday 26 September 2015 
+* 
+* Copyright Ali Reza Barkhordari 2016
+*/
